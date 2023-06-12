cc0a6d0e630737ddf790aea7be309db3efb80598
Vinicius Bastos <vclemente2@hotmail.com>
2023-06-12 12:12:33 -0300

    Criação do projeto e configurações iniciais.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyle from "./components/GlobalStyle";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>Hello World!</h1>
+    </>
+  );
+}
+
+export default App;
